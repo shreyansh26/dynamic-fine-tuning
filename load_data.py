e86df0e5b49de6449450b705e83e0e979f8a23e5
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2025-09-14 11:43:16 -0700

    Add functions to load benchmark datasets

--- load_data.py
+++ load_data.py
@@ -8,8 +8,36 @@ def load_numina_math():
     dataset = dataset.to_pandas()
     return dataset
 
+def load_math_500():
+    dataset = datasets.load_dataset("HuggingFaceH4/MATH-500", split="test")
+    dataset = dataset.select_columns(["problem", "solution", "answer"])
+    dataset = dataset.to_pandas()
+    return dataset
+
+def load_minerva_math():
+    dataset = datasets.load_dataset("math-ai/minervamath", split="test")
+    dataset = dataset.select_columns(["question", "answer"])
+    dataset = dataset.to_pandas()
+    return dataset
+
+def load_olympiad_bench():
+    dataset = datasets.load_dataset("Hothan/OlympiadBench", "OE_TO_maths_en_COMP", split="train")
+    dataset = dataset.select_columns(["question", "solution", "final_answer"])
+    dataset = dataset.to_pandas()
+    dataset['final_answer'] = dataset['final_answer'].apply(lambda x: x[0])
+    return dataset
+
 if __name__ == "__main__":
     dataset = load_numina_math()
-    dataset = dataset.sample(50000, random_state=1023)
-    os.makedirs("data", exist_ok=True)
-    dataset.to_csv("data/numina_math_50k.csv", index=False)
+    print(dataset.shape)
+    # dataset = dataset.sample(50000, random_state=1023)
+    # os.makedirs("data", exist_ok=True)
+    # dataset.to_csv("data/numina_math.csv", index=False)
+
+    # Testing for other datasets
+    dataset = load_math_500()
+    print(dataset.shape)
+    dataset = load_minerva_math()
+    print(dataset.shape)
+    dataset = load_olympiad_bench()
+    print(dataset.shape)
